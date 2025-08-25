162bec975d3c4f8ae7fac6e1c73ff8a575606ba8
Panda2609 <banda2609@gmail.com>
2025-08-25 18:06:51 -0400

    Creación del servidor, endpoints y modelos para canchas, usuarios, regiones y tipos de canchas

--- src/data/filter-data.js
+++ src/data/filter-data.js
@@ -1,20 +1,20 @@
 const Regiones = [
-  { value: 'arica_parinacota', label: 'Arica y Parinacota' },
-  { value: 'tarapaca', label: 'Tarapacá' },
-  { value: 'antofagasta', label: 'Antofagasta' },
-  { value: 'atacama', label: 'Atacama' },
-  { value: 'coquimbo', label: 'Coquimbo' },
-  { value: 'valparaiso', label: 'Valparaíso' },
-  { value: 'metropolitana', label: 'Metropolitana' },
-  { value: 'ohiggins', label: 'O’Higgins' },
-  { value: 'maule', label: 'Maule' },
-  { value: 'nuble', label: 'Ñuble' },
-  { value: 'biobio', label: 'Biobío' },
-  { value: 'araucania', label: 'La Araucanía' },
-  { value: 'los_rios', label: 'Los Ríos' },
-  { value: 'los_lagos', label: 'Los Lagos' },
-  { value: 'aysen', label: 'Aysén' },
-  { value: 'magallanes', label: 'Magallanes' }
+  { idRegion: 15, value: 'arica_parinacota', label: 'Arica y Parinacota' },
+  { idRegion: 1, value: 'tarapaca', label: 'Tarapacá' },
+  { idRegion: 2, value: 'antofagasta', label: 'Antofagasta' },
+  { idRegion: 3, value: 'atacama', label: 'Atacama' },
+  { idRegion: 4, value: 'coquimbo', label: 'Coquimbo' },
+  { idRegion: 5, value: 'valparaiso', label: 'Valparaíso' },
+  { idRegion: 13, value: 'metropolitana', label: 'Metropolitana' },
+  { idRegion: 6, value: 'ohiggins', label: 'O’Higgins' },
+  { idRegion: 7, value: 'maule', label: 'Maule' },
+  { idRegion: 16, value: 'nuble', label: 'Ñuble' },
+  { idRegion: 8, value: 'biobio', label: 'Biobío' },
+  { idRegion: 9, value: 'araucania', label: 'La Araucanía' },
+  { idRegion: 10, value: 'los_rios', label: 'Los Ríos' },
+  { idRegion: 11, value: 'los_lagos', label: 'Los Lagos' },
+  { idRegion: 12, value: 'aysen', label: 'Aysén' },
+  { idRegion: 14, value: 'magallanes', label: 'Magallanes' }
 ];
 
 const TiposCanchas = [
